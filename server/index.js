150431a1bedbd098f8314ecd6661c9fab1cd0a09
andreibratuleanu21 <andreibratuleangit@gmail.com>
2019-09-18 10:52:23 +0300

    add repo and creds to env

--- server/index.js
+++ server/index.js
@@ -3,15 +3,15 @@ const MongoClient = require('mongodb').MongoClient;
 const routes = require('./services/index');
 
 (async () => {
-	const client = await MongoClient.connect('mongodb://atlasClaudit:iKNies6GHGpiEooZ@167.71.3.14:27017', { useNewUrlParser: true, useUnifiedTopology: true });
+	const client = await MongoClient.connect(`mongodb://${process.env.DB_USER}:${process.env.DB_PASS}@127.0.0.1:27017`, { useNewUrlParser: true, useUnifiedTopology: true });
 	const serverConfig = {
 		db: client.db('firme'),
 		routes: {
 			'/api/companydata/:cif': routes.CompanyData
 		}
 	};
-	
+
 	const app = new Server(serverConfig);
-	
+
 	app.listen(2424);
 })();
